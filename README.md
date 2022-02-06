aacf9c45e73735983b4d55f3c54d33ea951979ff
Jim Carr <jfcarr@gmail.com>
2022-02-05 20:01:19 -0500

    New script, updated README

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # current-conditions-openweather
-Simple text output of current weather conditions, retrieved from OpenWeather API
+
+Simple text output of current weather conditions, retrieved from OpenWeather API.  (I use the output in a [Conky](https://github.com/brndnmtthws/conky) configuration)
+
+Before using the script, you'll need to sign up for access to the [OpenWeather API](https://openweathermap.org/api).  I use the free tier, which provides more than enough calls-per-month for personal use.  Once you've signed up, you'll be assigned an API key you can use in the script.
+
+Sample output:
+
+```
+Dayton
+  18° (feels like 8°)
+  clear sky, 0% cloud cover
+  -----
+  today's min,max: 18°,23°
+  sun rise,set: 07:40 AM,06:00 PM
+```
+
+## Daily Temperature Minimum and Maximum
+
+If you're running the script regularly, and you want to track daily high and low temperatures, create a `current_weather_ow.json` file in the script's working directory, with the following contents:
+
+```json
+{
+    "tracking_day": "1980-01-01",
+    "min_temp": -99,
+    "max_temp": -99
+}
+```
+
+Each time the script runs, it will compare the current date and temperature to the values in the .json file, and update them accordingly.  At midnight, the tracking values will be reset.
+
+## Measurements
+
+These are the default measurements in returned values.  Information comes from [here](https://openweathermap.org/current).
+
+Value | Measurement
+---------|----------
+Temperature | Kelvin
+Humidity | Percentage
+Wind Speed | Meters/second
+Time of Date Calculation (dt) | UTC, UNIX format
+
+The script automatically converts these to Imperial values.
---
+++ current_weather_ow.py
@@ -0,0 +1,132 @@
+#!/usr/bin/python3
+
+from datetime import datetime
+from os.path import exists
+import json
+import requests
+
+# User Settings
+latitude = 39.759444    # replace with your local latitude
+longitude = -84.191667  # replace with your local longitude
+api_key = 'replace-with-your-openweather-api-key'
+
+degree_sign = u'\N{DEGREE SIGN}'
+rounding_precision = 0  # decimal places
+config_file = 'current_weather_ow.json'
+
+def get_current_weather():
+	'''
+	Call the OpenWeather API and request the current weather conditions.
+	'''
+	url = f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}'
+	response = requests.get(url)
+
+	return response
+
+def convert_temperature(temp_k):
+	'''
+	Input format:  Kelvin
+
+	Output format: Fahrenheit
+	'''
+	temp_f = temp_k * 9/5 - 459.67
+
+	return round(temp_f, rounding_precision)
+
+def convert_windspeed(windspeed_ms):
+	'''
+	Input format:  meters/sec
+
+	Output format: miles/hour
+	'''
+	meters_per_hour = windspeed_ms * 60 * 60
+	miles_per_hour = meters_per_hour * 1 / 1609.344
+
+	return round(miles_per_hour, rounding_precision)
+
+def get_cardinal_direction(degrees):
+	'''
+	Given a cardinal direction in degrees, this will return a friendly direction description,
+	e.g., 'E' for 90 degrees.
+	'''
+	dirs = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]
+	ix = int((degrees + 11.25)/22.5)
+
+	return dirs[ix % 16]
+
+def get_min_max_temp(current_temp):
+	'''
+	Track minimum and maximum temperatures for the day.
+	'''
+	min_temp,max_temp = -99,-99
+
+	if exists(config_file):
+		with open(config_file, "r") as config_stream:
+			config_data = json.load(config_stream)
+			tracking_day = datetime.strptime(config_data["tracking_day"], "%Y-%m-%d")
+			current_day = datetime.today()
+			if tracking_day.date() != current_day.date():
+				tracking_day = current_day.date()
+				min_temp = current_temp
+				max_temp = current_temp
+			else:
+				tracking_day = tracking_day.date()
+				min_temp = config_data["min_temp"]
+				max_temp = config_data["max_temp"]
+				if current_temp < min_temp:
+					min_temp = current_temp
+				if current_temp > max_temp:
+					max_temp = current_temp
+
+		output_dict = { }
+		output_dict["tracking_day"] = str(tracking_day)
+		output_dict["min_temp"] = min_temp
+		output_dict["max_temp"] = max_temp
+
+		with open(config_file, "w") as config_stream:
+			json.dump(output_dict, config_stream, indent=4)
+		
+	return (round(min_temp,rounding_precision),round(max_temp,rounding_precision))
+
+def get_sunrise_sunset(result):
+	'''
+	Convert sunrise and sunset times from UNIX UTC to friendly strings, e.g., '07:42 AM' and '06:02 PM'
+	'''
+	sunrise_utc = result["sys"]["sunrise"]
+	sunset_utc = result["sys"]["sunset"]
+	timezone_offset = result["timezone"]
+
+	sunrise_value = datetime.utcfromtimestamp(sunrise_utc + timezone_offset)
+	sunset_value = datetime.utcfromtimestamp(sunset_utc + timezone_offset)
+
+	sunrise_str = str(sunrise_value.time().strftime("%I:%M %p"))
+	sunset_str = str(sunset_value.time().strftime("%I:%M %p"))
+
+	return (sunrise_str,sunset_str)
+
+if __name__ == "__main__":
+	result = json.loads(get_current_weather().text)
+
+	location = result["name"]
+	description = result["weather"][0]['description']
+	cloud_percent = result["clouds"]["all"]
+	actual_temperature = convert_temperature(result["main"]["temp"])
+	feels_like_temperature = convert_temperature(result["main"]["feels_like"])
+	wind_speed = convert_windspeed(result["wind"]["speed"])
+	try:
+		wind_speed_gust = convert_windspeed(result["wind"]["gust"])
+	except:
+		wind_speed_gust = 0
+	wind_cardinal = get_cardinal_direction(result["wind"]["deg"])
+	min_temp,max_temp = get_min_max_temp(actual_temperature)
+
+	(sunrise_str,sunset_str) = get_sunrise_sunset(result)
+
+	print(location)
+	print(f"  {int(actual_temperature)}{degree_sign} (feels like {int(feels_like_temperature)}{degree_sign})")
+	print(f"  {description}, {cloud_percent}% cloud cover")
+	print(f"  -----")
+	#print(f"wind: {wind_cardinal} at {int(wind_speed)} MPH (gusting to {int(wind_speed_gust)})")
+	if min_temp != -99:
+		print(f"  today's min,max: {int(min_temp)}{degree_sign},{int(max_temp)}{degree_sign}")
+	print(f"  sun rise,set: {sunrise_str},{sunset_str}")
